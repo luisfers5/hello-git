88ada8ad43ea6d2848d86a37f25d1553f9eb17dc
Luis Fernando Sanchez <lf.sanchezgutierrez@ugto.mx>
2023-10-05 10:26:45 -0600

    Subir cambios a github otra vez

--- hellogit3.py
+++ hellogit3.py
@@ -1,3 +1,4 @@
 print("Haciendo una prueba de como subir codigo a github")
 print("Ahora esta es la prueba de como bajar codigo de la nube a mi local")
 print("Ahora estamos probando si hay alguna diferencia de git pull origin main a si no tuviera los argumentos")
+print("Volviendo a subir a github con push ")
