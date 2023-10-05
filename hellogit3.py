3acaf942b7127daa6a7ae6b5f11936459ddd7871
Luis Fernando Sanchez <73081006+luisfers5@users.noreply.github.com>
2023-10-05 10:24:55 -0600

    Pull origin main, con argumentos

--- hellogit3.py
+++ hellogit3.py
@@ -1,3 +1,3 @@
 print("Haciendo una prueba de como subir codigo a github")
 print("Ahora esta es la prueba de como bajar codigo de la nube a mi local")
-
+print("Ahora estamos probando si hay alguna diferencia de git pull origin main a si no tuviera los argumentos")
